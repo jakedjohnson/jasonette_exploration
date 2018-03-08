79a183d19380f469b599fffb6275217bb168f7c2
Jacob Johnson <jake@revelry.co>
2018-03-08 00:04:10 -0800

    Sets up the api route

---
+++ lib/jasonette_exploration_web/controllers/home_controller.ex
@@ -0,0 +1,7 @@
+defmodule JasonetteExplorationWeb.HomeController do
+  use JasonetteExplorationWeb, :controller
+
+  def home(conn, _params) do
+    conn
+  end
+end
--- lib/jasonette_exploration_web/router.ex
+++ lib/jasonette_exploration_web/router.ex
@@ -2,10 +2,12 @@ defmodule JasonetteExplorationWeb.Router do
   use JasonetteExplorationWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/api", JasonetteExplorationWeb do
-    pipe_through :api
+    pipe_through(:api)
+
+    get("/", HomeController, :home)
   end
 end
